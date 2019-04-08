81032e797b6a18b16b2f412f4254347be10e8d6c
Marco Fabbri <marco.fabbri@drwolf.it>
2019-04-08 14:24:35 +0200

    :bug: fixed new buttons missing in refresh

--- src/lib/angular-editor-toolbar.component.ts
+++ src/lib/angular-editor-toolbar.component.ts
@@ -1,8 +1,8 @@
-import {Component, ElementRef, EventEmitter, Inject, Output, Renderer2, ViewChild} from "@angular/core";
-import {AngularEditorService} from "./angular-editor.service";
-import {HttpResponse} from "@angular/common/http";
-import {DOCUMENT} from "@angular/common";
-import {CustomClass, Font} from "./config";
+import {Component, ElementRef, EventEmitter, Inject, Output, Renderer2, ViewChild} from '@angular/core';
+import {AngularEditorService} from './angular-editor.service';
+import {HttpResponse} from '@angular/common/http';
+import {DOCUMENT} from '@angular/common';
+import {CustomClass, Font} from './config';
 
 @Component({
   selector: 'angular-editor-toolbar',
@@ -25,14 +25,14 @@ export class AngularEditorToolbarComponent {
   customClasses: CustomClass[];
 
   tagMap = {
-    BLOCKQUOTE: "indent",
-    A: "link"
+    BLOCKQUOTE: 'indent',
+    A: 'link'
   };
 
-  select = ["H1", "H2", "H3", "H4", "H5", "H6", "P", "PRE", "DIV"];
+  select = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'P', 'PRE', 'DIV'];
 
-  buttons = ["bold", "italic", "underline", "strikeThrough", "subscript", "superscript", "justifyLeft", "justifyCenter",
-    "justifyRight", "justifyFull", "indent", "outdent", "insertUnorderedList", "insertOrderedList", "link"];
+  buttons = ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', 'justifyLeft', 'justifyCenter',
+    'justifyRight', 'justifyFull', 'indent', 'outdent', 'insertUnorderedList', 'insertOrderedList', 'link', 'save', 'footnote'];
 
   @Output() execute: EventEmitter<string> = new EventEmitter<string>();
 
@@ -61,9 +61,9 @@ export class AngularEditorToolbarComponent {
       const result = this._document.queryCommandState(e);
       const elementById = this._document.getElementById(e + '-' + this.id);
       if (result) {
-        this._renderer.addClass(elementById, "active");
+        this._renderer.addClass(elementById, 'active');
       } else {
-        this._renderer.removeClass(elementById, "active");
+        this._renderer.removeClass(elementById, 'active');
       }
     });
   }
@@ -130,9 +130,9 @@ export class AngularEditorToolbarComponent {
       const elementById = this._document.getElementById(this.tagMap[e] + '-' + this.id);
       const node = nodes.find(x => x.nodeName === e);
       if (node !== undefined && e === node.nodeName) {
-        this._renderer.addClass(elementById, "active");
+        this._renderer.addClass(elementById, 'active');
       } else {
-        this._renderer.removeClass(elementById, "active");
+        this._renderer.removeClass(elementById, 'active');
       }
     });
   }
@@ -141,7 +141,7 @@ export class AngularEditorToolbarComponent {
    * insert URL link
    */
   insertUrl() {
-    const url = prompt("Insert URL link", 'http:\/\/');
+    const url = prompt('Insert URL link', 'http:\/\/');
     if (url && url !== '' && url !== 'http://') {
       this.editorService.createLink(url);
     }
@@ -150,7 +150,7 @@ export class AngularEditorToolbarComponent {
   /** insert color */
   insertColor(color: string, where: string) {
     this.editorService.insertColor(color, where);
-    this.execute.emit("");
+    this.execute.emit('');
   }
 
   /**
@@ -159,7 +159,7 @@ export class AngularEditorToolbarComponent {
    */
   setFontName(fontId: number): void {
     this.editorService.setFontName(this.fonts[fontId].name);
-    this.execute.emit("");
+    this.execute.emit('');
   }
 
   /**
@@ -168,7 +168,7 @@ export class AngularEditorToolbarComponent {
    *  */
   setFontSize(fontSize: string): void {
     this.editorService.setFontSize(fontSize);
-    this.execute.emit("");
+    this.execute.emit('');
   }
 
   /**
@@ -176,11 +176,11 @@ export class AngularEditorToolbarComponent {
    * @param m boolean
    */
   setEditorMode(m: boolean) {
-    const toggleEditorModeButton = this._document.getElementById("toggleEditorMode" + '-' + this.id);
+    const toggleEditorModeButton = this._document.getElementById('toggleEditorMode' + '-' + this.id);
     if (m) {
-      this._renderer.addClass(toggleEditorModeButton, "active");
+      this._renderer.addClass(toggleEditorModeButton, 'active');
     } else {
-      this._renderer.removeClass(toggleEditorModeButton, "active");
+      this._renderer.removeClass(toggleEditorModeButton, 'active');
     }
     this.htmlMode = m;
   }
@@ -190,7 +190,7 @@ export class AngularEditorToolbarComponent {
    */
   onFileChanged(event) {
     const file = event.target.files[0];
-      if (file.type.includes("image/")) {
+      if (file.type.includes('image/')) {
         this.editorService.uploadImage(file).subscribe(e => {
           if (e instanceof HttpResponse) {
             this.editorService.insertImage(e.body.imageUrl);
@@ -204,7 +204,7 @@ export class AngularEditorToolbarComponent {
    * Reset Input
    */
   fileReset() {
-    this.myInputFile.nativeElement.value = "";
+    this.myInputFile.nativeElement.value = '';
   }
 
   /**
