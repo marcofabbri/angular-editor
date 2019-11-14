ed0ef48a6bf700441b13d05d05e2626dd28e6764
Marco Fabbri <marco.fabbri@drwolf.it>
2019-11-14 15:59:37 +0100

    :bug: added tooltip module

--- src/lib/angular-editor.module.ts
+++ src/lib/angular-editor.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AngularEditorToolbarComponent } from './angular-editor-toolbar.component';
 import { AngularEditorComponent } from './angular-editor.component';
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, ReactiveFormsModule
+    CommonModule, FormsModule, ReactiveFormsModule,
+    TooltipModule
   ],
   declarations: [AngularEditorComponent, AngularEditorToolbarComponent],
   exports: [AngularEditorComponent, AngularEditorToolbarComponent]
